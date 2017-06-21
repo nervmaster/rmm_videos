6a08d070fc4f1df7e7e6054d4f12db3a2c5801a2
Henrique Pereira Borges <hpborges@gmail.com>
2017-06-20 22:14:29 -0300

    Overhaul
    O script funciona para todo tipo de video.
    Leva em conta que a entrada é no padrao h264

--- rmm.py
+++ rmm.py
@@ -7,6 +7,7 @@ import csv
 #	@author Victor Renan Covalski Junes <vrcjunes@inf.ufpel.edu.br>
 #	@author	Henrique Pereira Borges 	<hpborges@inf.ufpel.edu.br>
 
+# Funcao para pegar o SSIM e PSNR 
 def get_stats(video_stream, output):
 	arg = 'ffmpeg -i ' + video_stream + ' -i ' + output + ' -lavfi "ssim;[0:v][1:v]psnr" -f null -' 
 	stats = subprocess.Popen( shlex.split(arg) , stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
@@ -32,21 +33,65 @@ def get_stats(video_stream, output):
 		
 	return result
 
+def reencode(target, writer, profile = 'ultrafast'):
+	#width, height, bitrate and duration of the video #v:0 ignores the audio stream
+	arg = 'ffprobe -v quiet -print_format json -select_streams '\
+	      'v:0 -show_entries stream=r_frame_rate,height,width,bit_rate,duration {}'.format(target)
+	result = subprocess.Popen(shlex.split(arg),stdout=subprocess.PIPE)
+	result.wait()
+	file_info = json.loads(result.communicate()[0].decode("utf-8"))
 
+	#parsing metadata
+	original_bitrate = float(file_info['streams'][0]['bit_rate'])
+	resolution = '{}x{}'.format(file_info['streams'][0]['width'],file_info['streams'][0]['height'])
+	h = int(resolution.split('x')[0])
+	w = int(resolution.split('x')[1])
+
+	# Write target file to csv
+	row['archive'] = target
+	row['resolution'] = resolution
+	row['bitrate'] = original_bitrate
+	row['SSIM'] = 'N/A'
+	row['PSNR'] = 'N/A'
+	writer.writerow(row) 
+
+	#get frame rate
+	r_frame_rate = file_info['streams'][0]['r_frame_rate']
+	target_fps = 1 + int(int(r_frame_rate.split('/')[0]) / int(r_frame_rate.split('/')[1]))
+
+	#calculate target bitrate
+	target_bitrate = [1.0,0.8,0.6,0.4,0.2]
+	target_bitrate[:] = [int(x*original_bitrate) for x in target_bitrate]
+
+	for b in target_bitrate:
+		print '.',
+		sys.stdout.flush()
+		output = '{}x{}_{}.mp4'.format(h,w,b)
+		arg = 'ffmpeg -loglevel 0 -y -r {0} -i {1} -c:v libx264 -preset {2} -crf 22 '\
+		      '-x264-params keyint={3}:min-keyint={3}:scenecut=-1 '\
+			  '-b:v {4} -bufsize {4} -minrate {4} -maxrate {4} {5}'\
+			  ''.format(target_fps, video_stream, profile,i_frame_interval, b, output)
+		subprocess.Popen(shlex.split(arg)).wait()
+
+		# Gerar os dados de qualidade
+		stats = get_stats(target, output)
+
+		# Escrever no csv
+		row['archive'] = output
+		row['resolution'] = resolution
+		row['bitrate'] = x
+		row['SSIM'] = stats['ssim']
+		row['PSNR'] = stats['psnr']
+		writer.writerow(row)
+	print
 
 original_file = sys.argv[1] #filename
 
-profile = sys.argv[2]
-
 #width, height, bitrate and duration of the video #v:0 ignores the audio stream
 print 'Getting metadata'
-result = subprocess.Popen(['ffprobe', 
-						'-v','quiet',
-						'-print_format', 'json',
-						'-select_streams','v:0',
-						'-show_entries','stream=r_frame_rate,width,height,bit_rate,duration',
-						original_file],stdout=subprocess.PIPE)
-
+arg = 'ffprobe -v quiet -print_format json -select_streams v:0 -show_entries stream=r_frame_rate,height,width,bit_rate,duration {}'.format(original_file)
+result = subprocess.Popen(shlex.split(arg),stdout=subprocess.PIPE)
+result.wait()
 file_info = json.loads(result.communicate()[0].decode("utf-8"))
 
 
@@ -75,138 +120,43 @@ writer.writerow(row)
 r_frame_rate = file_info['streams'][0]['r_frame_rate']
 target_fps = 1 + int(int(r_frame_rate.split('/')[0]) / int(r_frame_rate.split('/')[1]))
 
-#calculate target bitrate
-target_bitrate = [1.0,0.8,0.6,0.4,0.2]
-target_bitrate[:] = [int(x*original_bitrate) for x in target_bitrate]
 
-video_stream =  'video_stream.mp4'
-
-print 'Spliting video'
 
 #separate video stream
-subprocess.Popen(['ffmpeg',
-				'-i',original_file,
-				'-an','-c','copy',video_stream, '-y'], stderr=subprocess.PIPE).wait() #redirecting stderr in order to make script run silently
+print 'Spliting video'
+video_stream =  'video_stream.mp4'
+arg = 'ffmpeg -loglevel 0 -i {} -an -c copy {} -y'.format(original_file, video_stream)
+subprocess.Popen(shlex.split(arg)).wait()
 
-print 'Spliting audio'
 #separate audio stream
-subprocess.Popen(['ffmpeg',
-				'-i',original_file,
-				'-r',str(target_fps),
-				'-vn','-c','copy','audio_stream.m4a', '-y'], stderr=subprocess.PIPE).wait()
+print 'Spliting audio'
+audio_stream = 'audio_stream.aac'
+arg = 'ffmpeg -loglevel 0 -i {} -r {} -vn -c copy {} -y'.format(original_file, target_fps, audio_stream)
+subprocess.Popen(shlex.split(arg)).wait()
+
+print 'Joining'
+target_1080 = 'target_1080.mp4'
+arg = 'ffmpeg -loglevel 0 -i {} -i {} -c:v copy -c:a aac -strict experimental {} -y'.format(video_stream, audio_stream, target_1080)
+subprocess.Popen(shlex.split(arg)).wait()
+
 
 #prepare string for x264-params
 i_frame_interval = target_fps*2
 
-print 'reecoding video'
-
-#encode for 1920x1080(1080p)
+print 'reencoding'
+# Resolucao original
 print '1080p'
-for x in target_bitrate:
-	print '.',
-	output = '{}_{}.mp4'.format(1080,x)
-	result = subprocess.Popen(['ffmpeg', '-y',
-	'-r',str(target_fps),
-	'-i',video_stream,
-	'-c:v','libx264',
-	'-preset', profile,
-	'-crf','22',
-	'-x264-params', 'keyint={0}:min-keyint={0}:scenecut=-1'.format(i_frame_interval),
-	'-b:v','{}'.format(x),
-	'-bufsize', '{}'.format(x),
-	'-minrate', '{}'.format(x),
-	'-maxrate', '{}'.format(x),
-	output], stderr=subprocess.PIPE).wait() #ffmpeg outputs to stderr
+reencode(target_1080, writer)
 
-	# Gerar os dados de qualidade
-	stats = get_stats(video_stream, output)
-
-	# Escrever no csv
-	row['archive'] = output
-	row['resolution'] = resolution
-	row['bitrate'] = x
-	row['SSIM'] = stats['ssim']
-	row['PSNR'] = stats['psnr']
-	writer.writerow(row)
-
-print
 print '720p'
-target = None
-for x in target_bitrate:
-	print '.',
-	output = '{}_{}.mp4'.format(720,x)
+# Encode target video
+target_720 = 'target_720.mp4'
+arg = 'ffmpeg -loglevel 0 -i {} -vf scale=-1:720 -strict experimental {} -y'.format(original_file, target_720)
+subprocess.Popen(shlex.split(arg), stderr=subprocess.PIPE).wait()
+reencode(target_720, writer)
 
-	result = subprocess.Popen(['ffmpeg', '-y',
-	'-r',str(target_fps),
-	'-i',video_stream,
-	'-c:v','libx264',
-	'-preset', profile,
-	'-crf','22',
-	'-x264-params', 'keyint={0}:min-keyint={0}:scenecut=-1'.format(i_frame_interval),
-	'-b:v','{}'.format(x),
-	'-bufsize', '{}'.format(x),
-	'-minrate', '{}'.format(x),
-	'-maxrate', '{}'.format(x),
-	'-vf', 'scale={}:{}'.format(2*h/3,2*w/3),
-	output], stderr=subprocess.PIPE).wait() #ffmpeg outputs to stderr
-	resolution = '{}x{}'.format(2*h/3, 2*w/3)
-	
-	# Usar o primeiro encode com a resolucao com taxa de bits original como comparacao
-	if(target == None):
-		target = output
-		row['SSIM'] = 'N/A'
-		row['PSNR'] = 'N/A'
-	else:
-		stats = get_stats(target, output)
-		row['SSIM'] = stats['ssim']
-		row['PSNR'] = stats['psnr']
-	
-	# Escrever no csv
-	row['archive'] = output
-	row['resolution'] = resolution
-	row['bitrate'] = x
-	writer.writerow(row)
-
-
-print 
 print '480p'
-target = None
-for x in target_bitrate: 
-	print '.',
-	output = '{}_{}.mp4'.format(480,x)
-
-	result = subprocess.Popen(['ffmpeg', '-y',
-	'-r',str(target_fps),
-	'-i',video_stream,
-	'-c:v','libx264',
-	'-preset', profile,
-	'-crf','22',
-	'-x264-params', 'keyint={0}:min-keyint={0}:scenecut=-1'.format(i_frame_interval),
-	'-b:v','{}'.format(x),
-	'-bufsize', '{}'.format(x),
-	'-minrate', '{}'.format(x),
-	'-maxrate', '{}'.format(x),
-	'-vf', 'scale={}:{}'.format(h/3,w/3),
-	output], stderr=subprocess.PIPE).wait()
-
-	resolution = '{}x{}'.format(h/3, w/3)
-	
-	# Usar o primeiro encode com a resolucao com taxa de bits original como comparacao
-	if(target == None):
-		target = output
-		row['SSIM'] = 'N/A'
-		row['PSNR'] = 'N/A'
-	else:
-		stats = get_stats(target, output)
-		row['SSIM'] = stats['ssim']
-		row['PSNR'] = stats['psnr']
-	
-	# Escrever no csv
-	row['archive'] = output
-	row['resolution'] = resolution
-	row['bitrate'] = x
-	writer.writerow(row)
-
-print
-print 'Done\nexiting...'
-arq.close()
+target_480 = 'target_480.mp4'
+arg = 'ffmpeg -loglevel 0 -i {} -vf scale=-1:480 -strict experimental {} -y'.format(original_file, target_480)
+subprocess.Popen(shlex.split(arg), stderr=subprocess.PIPE).wait()
+reencode(target_480, writer)
